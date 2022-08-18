9e3a56d12964f477b1708ea5aa8067fea927ed89
Jason Horsley <32709073+JasonHorsleyTech@users.noreply.github.com>
2022-08-18 16:58:38 -0500

    Initial commit

---
+++ resources/views/livewire/show-posts.blade.php
@@ -0,0 +1,7 @@
+<div class="p-8">
+    <h1 class="text-4xl text-blue-500 font-bold underline my-8">
+        Make a post?
+    </h1>
+
+    <textarea type="text" class="border rounded-sm p-2"></textarea>
+</div>

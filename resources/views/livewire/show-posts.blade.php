6acd4633ffc1e4e9801c093c6328116654da39ee
Jason Horsley <32709073+JasonHorsleyTech@users.noreply.github.com>
2022-08-18 17:09:48 -0500

    .

--- resources/views/livewire/show-posts.blade.php
+++ resources/views/livewire/show-posts.blade.php
@@ -1,6 +1,6 @@
 <div class="p-8">
-    <h1 class="text-4xl text-blue-500 font-bold underline my-8">
-        Make a post?
+    <h1 class="text-3xl my-2">
+        Make a post.
     </h1>
 
     <textarea type="text" class="border rounded-sm p-2"></textarea>
